3503949ace02640698d3ef9121abe2b11ba801f1
Pape98 <papisline2222@gmail.com>
2022-05-19 16:00:02 -0400

    changed footer 2

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,12 @@
 import { LinkedinFilled, GithubFilled } from '@ant-design/icons';
-import SenegalFlag from '../senegal-flag';
+// import SenegalFlag from '../senegal-flag';
 import './style.scss';
 
 const Footer = () => {
   return (
     <div id='footer'>
       <div className='footer__name'>Developed by Pape (2022)</div>
-      <div className='footer__love'>
-        <SenegalFlag />
-      </div>
+      <div className='footer__love'>{/* <SenegalFlag /> */}</div>
       <div className='footer__links'>
         <a href='https://www.linkedin.com/in/pape-sow-traore/' className='link'>
           <LinkedinFilled />
