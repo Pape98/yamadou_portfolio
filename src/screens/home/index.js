eda25fcad64792a4db757ba8b4780c894d215dd0
Pape98 <papisline2222@gmail.com>
2022-05-19 23:36:20 -0400

    changed home

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -9,17 +9,13 @@ const Intro = () => {
   return (
     <div id='home'>
       <div className='home__container'>
-        <div className='container__image'>
-          <img src='/assets/images/profile.png' alt='profile_image' />
-        </div>
         <div className='container__text'>
           <h1>
             Hi, I am <span className='text__name'>Pape</span>{' '}
             <span className='text__emoji'>&#128512;</span>
           </h1>
-          <div className='text__title'>Software Developer, Dartmouth Grad</div>
-          <div className='text__location'>
-            From Dakar, Senegal <SenegalFlag />
+          <div className='text__title'>
+            Software Developer @ Dartmouth College
           </div>
         </div>
       </div>
