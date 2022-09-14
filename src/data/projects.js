ed4b2ef07a533e85afa806c4e7aaa2a7e97a3230
Yamadou TRAORE <yamadou.traore.external@worldline.com>
2022-09-14 02:28:46 +0000

    yada yada

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,257 +3,145 @@
   And I cannot fork it.
 */
 
+
 const data = [
   {
     title: 'Laposte.net – Votre boîte mail',
     summary: 'National postal service of France mailing app.',
-    githubLink: 'https://apps.apple.com/fr/app/laposte-net-votre-bo%C3%AEte-mail/id149562915',
-    year: '2022',
-    tags: 'React,Microsoft Azure,Redux,NodeJS,PostgreSQL',
+    githubLink: '',
+    year: '2021 - Present',
+    tags: 'Firebase,CoreData,MVC,git,CocoaPods,Rest API',
     description: `The official Laposte.net mailing app brings the best of Laposte.net to your iPhone or iPad with robust security, real-time notifications,
                    multiple account support, and search that works across all your mail.`,
     isTeamProject: true,
     isWorkInProgress: true,
     appPreviewLink: 'https://apps.apple.com/fr/app/laposte-net-votre-bo%C3%AEte-mail/id149562915',
     preview:
+      'https://i.ibb.co/Z20CFdY/Screen-Shot-2022-09-13-at-20-09-36.png',
+    images: [
+      'https://i.ibb.co/Z20CFdY/Screen-Shot-2022-09-13-at-20-09-36.png',
+      'https://i.ibb.co/9Hj7cZw/Screen-Shot-2022-09-13-at-20-09-41.png',
       'https://i.ibb.co/7nDp3W4/Screen-Shot-2022-09-13-at-20-09-10.png',
-    images: [
-      'https://i.ibb.co/pnH5VfG/connectus1.png',
-      'https://i.ibb.co/b55ScBz/Screen-Shot-2022-05-15-at-4-55-16-PM.png',
-      'https://i.ibb.co/LpHDZk6/Screen-Shot-2022-05-15-at-4-55-49-PM.png',
-      'https://i.ibb.co/nBXw2GW/Screen-Shot-2022-05-15-at-4-57-30-PM.png',
-      'https://i.ibb.co/bLS7nWf/Screen-Shot-2022-05-15-at-4-54-52-PM.png',
+      'https://i.ibb.co/YjWD4Gk/Screen-Shot-2022-09-13-at-20-09-25.png',
     ],
   },
   {
-    title: 'ITC Vox',
+    title: 'Makeba Money ',
     summary:
-      'A system for Dartmouth College that allows faculty and staff to submit news, events, and announcements.',
+      'Pay and Send Money.',
     githubLink: '',
-    year: '2021',
-    tags: 'React,Redux,NodeJS,Ruby on Rails,PostgreSQL',
-    description: `Vox is a system for Dartmouth College that allows faculty and staff to submit news,
-                  events, and announcements. These submissions are then collated into daily digest emails.
-                  Each person receives an email with content targeted just for them, based on their
-                  membership to a set of target groups. The Dartmouth Office of Communications is able to
-                  log into our system and approve or reject submissions for the Vox Daily, as well as send
-                  out rejection emails detailing why the submission was not approved. The Office of
-                  Communications can then compile the daily briefing in the order they wish and send Vox
-                  out to the Dartmouth community.`,
+    year: '2019 - 2021',
+    tags: 'Clean Architecture,Storyboard,CoreData,Rest API,Firebase,git',
+    description: `Easily pay and send money using just your phone.
+                  Send and receive money between Makeba accounts in seconds directly from your phone.
+                  Pay in stores, take care of bills, shop online and more.`,
     isTeamProject: true,
     isWorkInProgress: false,
+    appPreviewLink: 'https://apps.apple.com/sn/app/makeba-money/id1458893785',
     preview:
-      'https://i.ibb.co/z8NSNdZ/Screen-Shot-2022-05-16-at-9-58-25-AM.png',
+      'https://i.ibb.co/F8p9Qp6/merchant.png',
     images: [
-      'https://i.ibb.co/z8NSNdZ/Screen-Shot-2022-05-16-at-9-58-25-AM.png',
-      'https://i.ibb.co/x7w2qxS/Screen-Shot-2022-05-16-at-9-58-00-AM.png',
-      'https://i.ibb.co/Z19yn4D/Screen-Shot-2022-05-16-at-9-58-08-AM.png',
+      'https://i.ibb.co/F8p9Qp6/merchant.png',
+      'https://i.ibb.co/HNYB5y4/merchant-03.pn',
+      'https://i.ibb.co/3fFy3RW/merchant-02.png',
     ],
   },
   {
-    title: 'Insect Invasion Model',
+    title: 'Makeba Merchant',
     summary:
-      'Vox is a system for Dartmouth College that allows faculty and staff to submit news, events, and announcements.',
+      'Makeba Merchant.',
     githubLink: '',
-    year: '2021',
-    tags: 'React,Redux,NodeJS,PostgreSQL',
+    year: '2019 - 2021',
+    tags: 'Clean Architecture,Storyboard,CoreData,Rest API,Firebase,Cocoapods,git',
     description:
-      'Fugiat duis dolor minim aute ad anim. Officia deserunt dolore consectetur sint ea pariatur deserunt veniam velit. Minim commodo do irure eu deserunt cupidatat cillum velit eu magna.',
+      'Running a business can be hard but getting paid should not be. Boost your business with fast mobile payments and low transaction fees.',
     isTeamProject: true,
     isWorkInProgress: false,
-    appPreviewLink: 'https://insect-invasion-dev.netlify.app/',
+    appPreviewLink: 'https://apps.apple.com/us/app/makeba-merchant/id1458671959',
     preview:
-      'https://i.ibb.co/85hGJMP/Screen-Shot-2022-05-15-at-5-44-36-PM.png',
+      'https://i.ibb.co/c2v83tZ/makeba-merchant-01.webp',
     images: [
-      'https://i.ibb.co/F4yYyJm/Screen-Shot-2022-05-15-at-5-44-18-PM.png',
-      'https://i.ibb.co/85hGJMP/Screen-Shot-2022-05-15-at-5-44-36-PM.png',
-      'https://i.ibb.co/bKHHjRr/Screen-Shot-2022-05-15-at-5-44-50-PM.png',
-      'https://i.ibb.co/80X5Q5z/Screen-Shot-2022-05-15-at-5-43-58-PM.png',
+      'https://i.ibb.co/c2v83tZ/makeba-merchant-01.webp',
+      'https://i.ibb.co/4JTxMc0/makeba-merchant-02.webp',
+      'https://i.ibb.co/Z6bFmyf/makeba-merchant-03.webp',
+      'https://i.ibb.co/9ny17yV/makeba-merchant-04.webp',
     ],
   },
   {
-    title: 'Fake Social Media Platform',
+    title: 'Xaalys',
     summary:
-      'A social media platform called Dalibook created for the DALI Lab at Dartmouth College.',
-    githubLink: 'https://github.com/Pape98/DaliChallenge',
-    year: '2020',
-    tags: 'NodeJS,Express',
-    description: `A social media platformn that is just for DALI members at Darmouth. 
-                  This website was created to complete a developer challenege. The 
-                  interface design and features mimic facebook. Users can create 
-                  posts, view other posts, check other profiles, search for other 
-                  DALI members.`,
-    isTeamProject: false,
-    isWorkInProgress: false,
-    appPreviewLink: 'https://dartdalibook.herokuapp.com/',
-    preview:
-      'https://i.ibb.co/tcKDGqj/Screen-Shot-2022-05-15-at-5-26-26-PM.png',
-    images: [
-      'https://i.ibb.co/tcKDGqj/Screen-Shot-2022-05-15-at-5-26-26-PM.png',
-      'https://i.ibb.co/phYN9zZ/Screen-Shot-2022-05-15-at-5-26-34-PM.png',
-      'https://i.ibb.co/hVHn1JT/Screen-Shot-2022-05-15-at-5-26-42-PM.png',
-      'https://i.ibb.co/jyRm0sx/Screen-Shot-2022-05-15-at-5-27-21-PM.png',
-      'https://i.ibb.co/Kjsrj2D/Screen-Shot-2022-05-15-at-5-27-44-PM.png',
-      'https://i.ibb.co/0f7x5ZJ/Screen-Shot-2022-05-15-at-5-26-16-PM.png',
-    ],
-  },
-  {
-    title: 'Community Forest Board Tree Tour',
-    summary: `A informational application that updates inhabitants of Aurburn and Lewsiton (ME) about the Community Forest Board and their activities
-      and news.`,
+      'French startup that wants to replace cash when you are handing out pocket money to your kids.',
     githubLink: '',
-    year: '2022 - Present',
-    tags: 'React,Redux,Firebase',
-    description: `The goal is to create an experience to introduce people to the notable trees of Lewiston and Auburn (virtual tree tour) and create
-                  Community Forest Board Website & identity. Features include ablity to add information about trees, virtual tree tour guide
-                  leveraging google map, and the ability to add news,events, and activies for the board staff and administrators.`,
-    preview: 'https://i.ibb.co/P9ggbrM/tree-tour.png',
-    isTeamProject: true,
-    isWorkInProgress: true,
-    images: ['https://i.ibb.co/P9ggbrM/tree-tour.png'],
-  },
-  {
-    title: 'CareAssist: Assistive Technology',
-    summary:
-      'An interface for nursing home caretakers to get live updates of the status of their patients with dementia.',
-    githubLink: 'https://github.com/Pape98/CareAssist-Web-Interface',
-    year: '2020',
-    tags: 'NodeJS,ExpressJS,MongoDB,PassportJS,Here API',
-    description: `An interface that allows caretakers to manage their patients. Caretakers can 
-                  add personal and health information related to their patients, send reminders 
-                  to take medication, and visualize different health metrics through a dashboard. 
-                  A geofence is also implemented, sending an alert to the caretaker when a patient 
-                  is out of the geofence (tracking done using wearable worn by patient)`,
-    isTeamProject: true,
-    isWorkInProgress: false,
-    appPreviewLink: 'https://careassist482.herokuapp.com/',
-    preview:
-      'https://i.ibb.co/3vzhvnK/Screen-Shot-2022-05-15-at-5-19-22-PM.png',
-    images: [
-      'https://i.ibb.co/3vzhvnK/Screen-Shot-2022-05-15-at-5-19-22-PM.png',
-      'https://i.ibb.co/MhSzZQp/Screen-Shot-2022-05-15-at-5-19-30-PM.png',
-      'https://i.ibb.co/85q24bK/Screen-Shot-2022-05-15-at-5-19-40-PM.png',
-      'https://i.ibb.co/Gv1TC6K/Screen-Shot-2022-05-15-at-5-19-47-PM.png',
-      'https://i.ibb.co/pn0HhHp/Screen-Shot-2022-05-15-at-5-20-00-PM.png',
-    ],
-  },
-  {
-    title: 'Video Tagging Tool',
-    summary:
-      "A tool to categorize, tag with keywords, and add timestamps to math videos for Texas A&M's Math department.",
-    githubLink: 'https://github.com/Pape98/TOPS-Video-Tool',
-    year: '2019-2020',
-    tags: 'Ruby on Rails,MongoDB,Phusion Passenger',
-    description: `The department was going through an archive of videos (3000 hours). 
-                  This application replaced their previously used spreadsheets and forms and 
-                  provided a single repo to both edit and retrieve information.
-                  Features include the abiltity to authenticate, add metadata to videos, attach 
-                  worksheets and documents to videos, review videos by filling out usability 
-                  rubric, and search through videos based on different fields and keywords.`,
+    year: '2018 - 2019',
+    tags: 'MVVM,Realm,Rest API, Firebase,Cocoapods,git',
+    description: ` Anybody who is older than 10 years old can create a Xaalys account, get a debit card and manage pocket money. Challenger banks are nothing new, but they're still mostly targeted towards adults.`,
     isTeamProject: false,
     isWorkInProgress: false,
-    appPreviewLink: 'https://tops-video-tool.herokuapp.com/',
+    appPreviewLink: '',
     preview:
-      'https://i.ibb.co/DV1gYZT/Screen-Shot-2022-05-15-at-5-07-13-PM.png',
+      'https://i.ibb.co/KqPKJQW/xaalys.jpg',
     images: [
-      'https://i.ibb.co/DV1gYZT/Screen-Shot-2022-05-15-at-5-07-13-PM.png',
-      'https://i.ibb.co/vkSNRxz/Screen-Shot-2022-05-15-at-5-07-29-PM.png',
-      'https://i.ibb.co/BL9zk37/Screen-Shot-2022-05-15-at-5-07-44-PM.png',
-      'https://i.ibb.co/CKJb1Ff/Screen-Shot-2022-05-15-at-5-07-51-PM.png',
+      'https://i.ibb.co/KqPKJQW/xaalys.jpg',
+      'https://i.ibb.co/vmp9dwY/xaalys-1.png',
+      'https://i.ibb.co/1r9WdZs/xaalys.png',
+      'https://i.ibb.co/WPfdKVD/xaalysss.png',
     ],
   },
   {
-    title: 'Student Organization Management Tool',
-    summary:
-      'Application that allows student-led organizations to manage day-to-day operations.',
-    githubLink: 'https://github.com/Pape98/Slide-Website',
-    year: '2017-2018',
-    tags: 'NodeJS,ExpressJS,MongoDB,PassportJS',
-    description: `Many on-campus college organizations that are led by students conduct day-to-day 
-                  activities. Members of are often required to get a certain number of points by 
-                  attending social and service events. This application centralizes all of the 
-                  tools needed to run the organization. Features include members and staff interface
-                  points and appeals review and submission, volunteer sign-up sheets, and many others.`,
-    isTeamProject: false,
-    isWorkInProgress: false,
-    appPreviewLink: 'https://tamuslide.herokuapp.com/home',
-    preview:
-      'https://i.ibb.co/grgxdP0/Screen-Shot-2022-05-15-at-5-02-43-PM.png',
-    images: [
-      'https://i.ibb.co/bHr3Wqv/Screen-Shot-2022-05-15-at-5-02-28-PM.png',
-      'https://i.ibb.co/grgxdP0/Screen-Shot-2022-05-15-at-5-02-43-PM.png',
-      'https://i.ibb.co/k9TnKLY/Screen-Shot-2022-05-15-at-5-03-04-PM.png',
-      'https://i.ibb.co/23bQhpd/Screen-Shot-2022-05-15-at-5-02-06-PM.png',
-    ],
-  },
-  {
-    title: 'Qualitative Analysis Tool',
-    summary:
-      'An academic research tool to search through tagged interview quotes.',
-    githubLink: 'https://github.com/Pape98/Qualitative-Analysis-Tool',
-    year: '2022',
-    tags: 'React,Redux,Firebase,Drive API',
-    description: `For my master's research, I had to conduct some user interviews and analyze the data. Analyzing consisted
-          adding codes/tags to important quotes in order to generate categories and subsequently themes. The tagging
-          process was done using Google Docs by just adding comments to quotes. The next step was to combine all the 
-          quotes with similar codes/tags. Instead of manuallay combining the quotes, I extracted all the quotes along
-          with their tags from different documenst using the Google Drive API and created this interface to be able 
-          to quickly search access quotes with wanted tags.`,
-    isTeamProject: false,
-    isWorkInProgress: false,
-    appPreviewLink: 'https://research-needfinfing.web.app/',
-    preview:
-      'https://i.ibb.co/SN66bwp/Screen-Shot-2022-05-16-at-9-37-47-AM.png',
-    images: [
-      'https://i.ibb.co/SN66bwp/Screen-Shot-2022-05-16-at-9-37-47-AM.png',
-      'https://i.ibb.co/YtBb79Q/Screen-Shot-2022-05-16-at-9-38-00-AM.png',
-      'https://i.ibb.co/MCM44JG/Screen-Shot-2022-05-16-at-9-38-40-AM.png',
-    ],
-  },
-  {
-    title: 'Community Garden',
-    summary:
-      'A self-tracking application that lets user track data with a community in visualize data in a qualitative way. ',
+    title: 'Sama Ndaw',
+    summary: `Mobile application that contributes to improving the Sexual and Reproductive Health of young people.`,
     githubLink: '',
-    year: '2022 - Present',
-    tags: 'iOS,Swift,SwiftUI,SpriteKit,Firebase,React,Redux',
-    description: `This application is a work in progress for my master's research. The idea is to create a virrual environment where
-                  each member of the community is tracking something different. The environment's changes are directly tied to the user's actions.
-                  Is the user making progress? Are they meetings their goals. In our scenario, each user will be growing their own garden with trees
-                  and flowers. The growth of their garden is dependent on them getting for example a certain number of steps. The community view is
-                  presented not on the user's phone, but on a big ambient display, allowing users to walk by and visualizing the general state of 
-                  people which is direct mapping to the general state of the communit garden. My research aims to find ways of qualitative 
-                  and metaphorical ways of visualizing data and see the impacts (and maybe the benefits) of tracking as a community.`,
+    year: '2017 - 2018',
+    tags: 'MVC,Storyboard,Realm,Cocoapods',
+    description: ``,
+    preview: 'https://i.ibb.co/hdzX0rY/sama-ndaw-02.jpg',
+    appPreviewLink: 'https://apps.apple.com/ar/app/sama-ndaw/id1364199351',
     isTeamProject: false,
-    isWorkInProgress: true,
-    preview: '',
-    images: [],
-  },
-  {
-    title: 'Sign Language AR',
-    summary: 'A virtual reality game to teach non-deaf people sign language.',
-    githubLink: '',
-    year: '2021',
-    tags: 'Virtual Reality,C#,Unity Engine',
-    description: `A virtual reality application that teaches sign language through gamification. 
-                  Players have the option to sign both letters and words. The player is in a 
-                  space scene and a robot serves as the teacher and provides visual feedback. 
-                  Moreover, the game also includes an onboarding system and different learning 
-                  modules.`,
-    isTeamProject: true,
     isWorkInProgress: false,
-    preview:
-      'https://i.ibb.co/sbKcrQh/Screen-Shot-2022-05-16-at-2-08-46-AM.png',
     images: [
-      'https://i.ibb.co/9y3FpJL/Screen-Shot-2022-05-16-at-2-09-35-AM.png',
-      'https://i.ibb.co/H7nB7hx/Screen-Shot-2022-05-16-at-2-09-18-AM.png',
-      'https://i.ibb.co/sbKcrQh/Screen-Shot-2022-05-16-at-2-08-46-AM.png',
-      'https://i.ibb.co/3p5mCTC/Screen-Shot-2022-05-16-at-2-08-38-AM.png',
-      'https://i.ibb.co/1K8sgpF/Screen-Shot-2022-05-16-at-2-08-14-AM.png',
+      'https://i.ibb.co/hdzX0rY/sama-ndaw-02.jpg',
+      'https://i.ibb.co/2nfvwBV/sama-ndaw-01.jpg',
+      'https://i.ibb.co/ssLZpCL/sama-ndaw-03.jpg',
     ],
   },
+  {
+    title: 'APIX',
+    summary:
+      'The Apix - Reforms application is a tool developed by Apix to allow its users to quickly access the effective focus doing business reforms.',
+    githubLink: '',
+    year: '2018',
+    tags: 'MVC,Storyboard,Realm,Cocoapods',
+    description: '',
+    isTeamProject: false,
+    isWorkInProgress: false,
+    appPreviewLink: 'https://apps.apple.com/ar/app/apix-r%C3%A9formes/id1401094687',
+    preview:
+      'https://i.ibb.co/HzvZqS9/apix-02.webp',
+    images: [
+      'https://i.ibb.co/HzvZqS9/apix-02.webp',
+      'https://i.ibb.co/mRyQG5N/apix-01.webp',
+      'https://i.ibb.co/ZNyYBbs/apix-03.webp',
+      'https://i.ibb.co/XSB7ymR/apix-04.webp',
+    ],
+  },
+  {
+    title: 'Manuel Universitaire',
+    summary:
+      'The Manual offers you in digital version all of the documents of your faculty',
+    githubLink: '',
+    year: '2018',
+    tags: 'MVC,Storyboard,Realm,Cocoapods',
+    description: '',
+    isTeamProject: false,
+    isWorkInProgress: false,
+    appPreviewLink: '',
+    preview:
+      'https://i.ibb.co/VjFWh2J/Manuel.webp',
+    images: [
+      'https://i.ibb.co/VjFWh2J/Manuel.webp',
+      'https://i.ibb.co/v4LgKvB/manuel-02.webp',
+    ],
+  }
 ];
 
 export default data;
-
-
